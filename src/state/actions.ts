295044c1beaddccd685671a5e3ef5d7e7d41c8a7
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-04-24 17:53:20 -0700

    started using redux

---
+++ src/state/actions.ts
@@ -0,0 +1,24 @@
+// Define two actions -- adding a list; adding a task
+
+export type Action =
+  | {
+      type: "ADD_LIST"
+      payload: string
+    }
+  | {
+      type: "ADD_TASK"
+      payload: { text: string; listId: string }
+    }
+
+export const addTask = (text: string, listId: string): Action => ({
+  type: "ADD_TASK",
+  payload: {
+    text,
+    listId,
+  },
+})
+
+export const addList = (text: string): Action => ({
+  type: "ADD_LIST",
+  payload: text,
+})
