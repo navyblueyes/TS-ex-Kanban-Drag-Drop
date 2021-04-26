f4a164acab43f9986ffc20e665069a2c535f3ddf
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-04-26 14:52:01 -0700

    moving interaction -- updated CustomDragLayer to include Card component

--- src/CustomDragLayer.tsx
+++ src/CustomDragLayer.tsx
@@ -1,25 +1,28 @@
 import { useDragLayer } from "react-dnd"
 import { Column } from "./Column"
-import {
-  CustomDragLayerContainer,
-  DragPreviewWrapper
-} from "./styles"
+import { CustomDragLayerContainer, DragPreviewWrapper } from "./styles"
 import { useAppState } from "./state/AppStateContext"
+import { Card } from "./Card"
 
 export const CustomDragLayer = () => {
   const { draggedItem } = useAppState()
   const { currentOffset } = useDragLayer((monitor) => ({
-    currentOffset: monitor.getSourceClientOffset()
+    currentOffset: monitor.getSourceClientOffset(),
   }))
 
   return draggedItem && currentOffset ? (
     <CustomDragLayerContainer>
       <DragPreviewWrapper position={currentOffset}>
-        <Column
-          id={draggedItem.id}
-          text={draggedItem.text}
-          isPreview
-        />
+        {draggedItem.type === "COLUMN" ? (
+          <Column id={draggedItem.id} text={draggedItem.text} isPreview />
+        ) : (
+          <Card
+            columnId={draggedItem.columnId}
+            id={draggedItem.id}
+            text={draggedItem.text}
+            isPreview
+          />
+        )}
       </DragPreviewWrapper>
     </CustomDragLayerContainer>
   ) : null
