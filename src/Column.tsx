4eaebdb1615ce85957405ad19beb7bf68c21c9ca
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-04-26 17:48:44 -0700

    moving interaction -- updated Column to register moving tasks

--- src/Column.tsx
+++ src/Column.tsx
@@ -6,7 +6,7 @@ import { AddNewItem } from "./AddNewItem"
 import { useItemDrag } from "./utils/useItemDrag"
 import { useDrop } from "react-dnd"
 import { isHidden } from "./utils/isHidden"
-import { moveList, addTask } from "./state/actions"
+import { moveList, addTask, setDraggedItem, moveTask } from "./state/actions"
 
 type ColumnProps = {
   text: string
@@ -19,7 +19,7 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
   const tasks = getTasksByListId(id)
   const ref = useRef<HTMLDivElement>(null)
   const [, drop] = useDrop({
-    accept: "COLUMN",
+    accept: ["COLUMN", "CARD"],
     hover() {
       if (!draggedItem) {
         return
@@ -30,6 +30,16 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
         }
 
         dispatch(moveList(draggedItem.id, id))
+      } else {
+        if (draggedItem.columnId === id) {
+          return
+        }
+        if (tasks.length) {
+          return
+        }
+
+        dispatch(moveTask(draggedItem.id, null, draggedItem.columnId, id))
+        dispatch(setDraggedItem({ ...draggedItem, columnId: id }))
       }
     },
   })
